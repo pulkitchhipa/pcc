6a466bbd45da21c127c1e41008bedd983ea5df72
pulkitchhipa <pulkitchhipa01@gmail.com>
2022-07-29 00:29:06 +0530

    sort an array of 0,1,2

---
+++ sort_array_0_1_2.java
@@ -0,0 +1,31 @@
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        int count0=0;
+        int count1=0;
+        int count2=0;
+        
+        for(int i=0;i<n;i++){
+            
+            if(a[i]==0){
+                count0++; 
+            }
+            if(a[i]==1){
+                count1++;
+            }
+            if(a[i]==2){
+                count2++;
+            }
+        }
+        for(int i=0;i<count0;i++){
+            a[i]=0;
+        }
+        for(int i=count0;i<(count0+count1);i++){
+            a[i]=1;
+        }
+        for(int i=(count0+count1);i<n;i++){
+            a[i]=2;
+        }
+    }
+}
