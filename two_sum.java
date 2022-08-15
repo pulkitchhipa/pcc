a69336f1d448db8924affe205582af3ae5ba9455
pulkitchhipa <pulkitchhipa01@gmail.com>
2022-08-16 00:13:53 +0530

    lc --> 1. Two Sum

--- two_sum.java
+++ two_sum.java
@@ -1,14 +1,17 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int i,j;
-        for(i=0,j=1; j<nums.length; j++) {
-            if(nums[i] != nums[j]) {
-                nums[++i] = nums[j];
-            }
-            else {
-                continue;
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int neededValue = target - nums[i];
+            if (map.containsKey(neededValue)) {
+                result[0] = i;
+                result[1] = map.get(neededValue);
+                break;
+            } else {
+                map.put(nums[i], i);
             }
         }
-        return i+1;
+        return result;
     }
 }
